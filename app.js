096bb6542e1b233c566b23f838bbe821f5ea1fe1
Luke Rebiro <68423198+lukerebeirowebdev@users.noreply.github.com>
2021-07-01 12:47:02 +0100

    added swich statement for user and computer choice

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const userScore = 0;
 const computerScore = 0;
-const userScore_span = document.createElementById("user-score");
-const computerScore_span = document.createElementById("computer-score");
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
@@ -9,3 +9,41 @@ const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 const lizard_div = document.getElementById("l");
 const spock_div = document.getElementById("sp");
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's', 'l', 'sp'];
+    const randomNumber = Math.floor(Math.random() * 5);
+    return choices[randomNumber];
+}
+
+function game(userChoice) {
+    const ComputerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+
+    }
+}
+
+function main() {
+
+    rock_div.addEventListener('click', function() {
+        game("r");
+    })
+
+    paper_div.addEventListener('click', function() {
+        game("p");
+    })
+
+    scissors_div.addEventListener('click', function() {
+        game("s");
+    })
+
+    lizard_div.addEventListener('click', function() {
+        game("l");
+    })
+
+    spock_div.addEventListener('click', function() {
+        game("sp");
+    })
+}
+
+main();
